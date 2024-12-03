dbe0f2a6a21ea5549459ad57eaa372fc23f3c1ea
Kajetan Krasoń <kajetan@czaplinek.pl>
2024-12-03 21:41:32 +0100

    Need to change diff > 3 in day2, level 2

--- days/day2.go
+++ days/day2.go
@@ -1,5 +1,97 @@
 package main
 
-func main() {
+import (
+	"advent-of-code-2024/utilities"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func isSafe(report []int) bool {
+	isIncreasing := true
+	isDecreasing := true
+
+	for i := 1; i < len(report); i++ {
+		difference := math.Abs(float64(report[i] - report[i-1]))
+		if difference > 3 || difference < 1 {
+			return false
+		}
+		if report[i] > report[i-1] {
+			isDecreasing = false
+		}
+		if report[i] < report[i-1] {
+			isIncreasing = false
+		}
+	}
+	return isIncreasing || isDecreasing
+}
+
+func removeElemFromSlice(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func isSafeWithLevelDampener(report []int) bool {
+	isIncreasing := true
+	isDecreasing := true
+
+	for i := 1; i < len(report); i++ {
+		difference := math.Abs(float64(report[i] - report[i-1]))
+		if difference > 3 {
+			return false
+		}
+		if difference < 1 {
+			report = removeElemFromSlice(report, i)
+			return isSafe(report)
+		}
+		if report[i] > report[i-1] {
+			isDecreasing = false
+		}
+		if report[i] < report[i-1] {
+			isIncreasing = false
+		}
+		if !isIncreasing && !isDecreasing {
+			report = removeElemFromSlice(report, i)
+			return isSafe(report)
+		}
+	}
+	return isIncreasing || isDecreasing
+}
+
+func convertStringArrToIntArr(strArr []string) []int {
+	var intArr []int
+	for _, elem := range strArr {
+		intElem, err := strconv.Atoi(elem)
+		utilities.Check(err)
+		intArr = append(intArr, intElem)
+	}
+	return intArr
+}
+
+func main() {
+	file, err := os.Open("inputs/day2test.txt")
+	utilities.Check(err)
+	scanner := bufio.NewScanner(file)
+	var fullDataArr [][]string
+	for scanner.Scan() {
+		lineArr := strings.Fields(scanner.Text())
+		fullDataArr = append(fullDataArr, lineArr)
+	}
+	var countTrues1 int
+	var countTrues2 int
+	for _, report := range fullDataArr {
+		intReport := convertStringArrToIntArr(report)
+		safeStatement := isSafe(intReport)
+		if safeStatement {
+			countTrues1++
+		}
+		safeStatement = isSafeWithLevelDampener(intReport)
+		if safeStatement {
+			countTrues2++
+		}
+	}
+	fmt.Println(countTrues1)
+	fmt.Println(countTrues2)
 }
