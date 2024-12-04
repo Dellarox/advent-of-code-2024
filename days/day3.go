4706342f6ab54448785927ab7ba52efe347290fb
Kajetan Krason <kajetan.krason@estigiti.com>
2024-12-04 12:41:25 +0100

    Day 3 ready

--- days/day3.go
+++ days/day3.go
@@ -13,13 +13,39 @@ func main() {
 	dataStr := string(dat)
 
 	r, _ := regexp.Compile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
-	multipliactionsArr := r.FindAllString(dataStr, -1)
+	multiplicationsArr := r.FindAllString(dataStr, -1)
 
-	r, _ = regexp.Compile(`[0-9]{1,3}`)
+	rCheckNumbers, _ := regexp.Compile(`[0-9]{1,3}`)
 	var result int
-	for _, mul := range multipliactionsArr {
-		numbers := utilities.ConvertStringArrToIntArr(r.FindAllString(mul, 2))
+	for _, mul := range multiplicationsArr {
+		numbers := utilities.ConvertStringArrToIntArr(rCheckNumbers.FindAllString(mul, 2))
 		result += numbers[0] * numbers[1]
 	}
 	fmt.Println(result)
+
+	r, _ = regexp.Compile(`don't|do|mul\([0-9]{1,3},[0-9]{1,3}\)`)
+	multiplicationsAndInstrArr := r.FindAllString(dataStr, -1)
+
+	isDont := false
+	multiplicationsArr = nil
+	for _, multOrInstr := range multiplicationsAndInstrArr {
+		if multOrInstr == "do" {
+			isDont = false
+			continue
+		}
+		if multOrInstr == "don't" {
+			isDont = true
+			continue
+		}
+		if !isDont {
+			multiplicationsArr = append(multiplicationsArr, multOrInstr)
+		}
+	}
+
+	var result2 int
+	for _, mul := range multiplicationsArr {
+		numbers := utilities.ConvertStringArrToIntArr(rCheckNumbers.FindAllString(mul, 2))
+		result2 += numbers[0] * numbers[1]
+	}
+	fmt.Println(result2)
 }
